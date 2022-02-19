2a232e1489ff7b6e7704a186ef19d6583a9c02a6
Michele Rizzello <mrizzello@gmail.com>
2022-02-19 16:58:01 +0100

    Changed svg to png export process.

--- src/app/services/export.service.ts
+++ src/app/services/export.service.ts
@@ -1,73 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Canvg } from 'canvg';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExportService {
 
-  private width: number = 0;
-  private height: number = 0;
-  private svgElement: any = null;
-  private blobUrl: string = '';
-  private canvas: any = null;
-  private ctx: any = null;
-  private png: any = null;
+  public exportPNGRunning: boolean = false;
 
   constructor() { }
 
-  async saveAsPng(svgElement: SVGGraphicsElement) {
-    this.svgElement = svgElement;
-    await this.generateSvgUrl()
-    await this.generateCanvas();
-    await this.generatePng();
-    this.download();
-  }
-
-  generateSvgUrl() {
-    return new Promise((resolve) => {
-      let { width, height } = this.svgElement.getBBox();
-      this.width = width;
-      this.height = height;
-      let clonedSvgElement = this.svgElement.cloneNode(true);
-      let svgString = new XMLSerializer().serializeToString(clonedSvgElement);
-      let svg = new Blob([svgString], { type: "image/svg+xml;charset=utf-8" });
-      let DOMURL = self.URL || self.webkitURL || self;
-      let url = DOMURL.createObjectURL(svg);
-      this.blobUrl = url;
-      resolve(true);
+  saveAsPng(svgElement: SVGGraphicsElement) {
+    this.exportPNGRunning = true;
+    this.exportPNG(svgElement, () => {
+      this.exportPNGRunning = false;
     });
   }
 
-  generateCanvas() {
-    return new Promise((resolve) => {
-      this.canvas = document.createElement('canvas');
-      this.canvas.width = this.width;
-      this.canvas.height = this.height;
-      this.ctx = this.canvas.getContext("2d");
-      resolve(true);
+  exportPNG(svgElement: SVGGraphicsElement, callback: any) {
+    this.exportCanvas(svgElement, (canvas: HTMLCanvasElement) => {
+      canvas.toBlob((blob) => {
+        var link = document.createElement("a");
+        link.download = "EphemeraKit.png";
+        link.href = URL.createObjectURL(blob);
+        link.click();
+        callback();
+      });
     });
   }
 
-  generatePng() {
-    return new Promise((resolve) => {
-      let img = new Image();
-      let canvas = this.canvas;
-      let ctx = this.ctx;
-      let that = this;
-      img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        let png = document.createElement('a');
-        png.href = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
-        png.download = 'MyPliego.png';
-        that.png = png;
-        resolve(true);
-      };
-      img.src = this.blobUrl;
-    });
-  }
-
-  download() {
-    this.png.click();
+  async exportCanvas(svgElement: SVGGraphicsElement, callback: any) {
+    let { width, height } = svgElement.getBBox();
+    let clonedSvgElement = svgElement.cloneNode(true);
+    let svgString = new XMLSerializer().serializeToString(clonedSvgElement);
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+    const v = Canvg.fromString(ctx, svgString);
+    await v.render();
+    callback(canvas);
   }
 
 }
